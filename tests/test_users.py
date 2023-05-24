736a517035b781281f061cf4192d559d3c5702ec
tiagocollot <collot.tiago1@gmail.com>
2023-05-24 15:36:48 +0100

    (test_users): - created User class - created tests for user construct/identical/format to string

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,27 @@
 from lib.users import User
 
 """
-User constructs with an id, username and password
+User constructs with an id, username, email and password
 """
 def test_user_constructs():
-    user = User(1, "john_doe", "password123")
-    assert User.id == 1
-    assert User.username == "john_doe"
-    assert User.password == "password123"
+    user = User(1, 'JohnDoe', 'johndoe@example.com', 'password123')
+    assert user.id == 1
+    assert user.username == 'JohnDoe'
+    assert user.email == 'johndoe@example.com'
+    assert user.password == 'password123'
+
+"""
+We can compare two identical users
+And have them be equal
+"""
+def test_users_are_equal():
+    user1 = User(1, 'JohnDoe', 'johndoe@example.com', 'password123')
+    user2 = User(1, 'JohnDoe', 'johndoe@example.com', 'password123')
+    assert user1 == user2
+    
+"""
+We can format users to strings nicely
+"""
+def test_user_format_nicely():
+    user = User(1, 'JohnDoe', 'johndoe@example.com', 'password123')
+    assert str(user) == "User(1, JohnDoe, johndoe@example.com, password123)"
