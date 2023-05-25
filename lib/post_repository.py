67e60544fd6b28632cf83cd820c6f6621ce75dab
tiagocollot <collot.tiago1@gmail.com>
2023-05-25 13:54:46 +0100

    chitter_project(post): - created post(CRUD) - created and implemented post_repository TDD

--- lib/post_repository.py
+++ lib/post_repository.py
@@ -1,4 +1,5 @@
 from lib.post import Post
+from datetime import datetime
 
 class PostRepository():
     def __init__(self, connection):
@@ -8,6 +9,18 @@ class PostRepository():
         rows = self._connection.execute('SELECT * FROM posts')
         posts = []
         for row in rows:
-            item = Post(row["id"], row["content"], row["post_time"], row["user_id"])
+            post_time = datetime.strftime(row["post_time"], "%Y-%m-%d %H:%M:%S")
+            item = Post(row["id"], row["content"], post_time, row["user_id"])
             posts.append(item)
         return posts
+    
+    def create(self, post):
+        self._connection.execute('INSERT INTO posts (content, post_time, user_id) VALUES (%s, %s, %s)', [post.content, post.post_time, post.user_id])      
+        return None
+    
+    def find(self, post_id):
+        rows = self._connection.execute(
+                'SELECT * from posts WHERE id = %s', [post_id])
+        row = rows[0]
+        post_time = datetime.strftime(row["post_time"], "%Y-%m-%d %H:%M:%S")
+        return Post(row["id"], row["content"], post_time,  row["user_id"])
--- tests/test_post_repository.py
+++ tests/test_post_repository.py
@@ -17,43 +17,30 @@ def test_get_all_posts(db_connection): # See conftest.py to learn what `db_conne
         Post(2, 'This is my first peep!', '2023-05-19 10:30:00', 2)
     ]
     
-#     """
-# When we call AlbumRepository#create
-# We get a new record in the database.
-# """
-    # expected_posts = [
-    #     Post(1, 'Hello, world!', '2023-05-19 09:00:00', 1),
-    #     Post(2, 'This is my first peep!', '2023-05-19 10:30:00', 2)
-    # ]
+"""
+When we call PostRepository#create
+We get a new record in the database.
+"""
+def test_create_record(db_connection):
+    db_connection.seed("seeds/chitter_database.sql")
+    repository = PostRepository(db_connection)
 
-    # # Assert on the results
-    # assert len(posts) == len(expected_posts)  # Check if the number of posts is the same
+    repository.create(Post(None, 'This is my second peep!', '2023-05-19 10:35:00', 2))
 
-    # for post, expected_post in zip(posts, expected_posts):
-    #     assert post.id == expected_post.id
-    #     assert post.content == expected_post.content
-    #     assert post.post_time == expected_post.post_time
-    #     assert post.user_id == expected_post.user_id
-# def test_create_record(db_connection):
-#     db_connection.seed("seeds/music_web_app.sql")
-#     repository = AlbumRepository(db_connection)
+    result = repository.all()
+    assert result == [
+        Post(1, 'Hello, world!', '2023-05-19 09:00:00', 1),
+        Post(2, 'This is my first peep!', '2023-05-19 10:30:00', 2),
+        Post(3, 'This is my second peep!', '2023-05-19 10:35:00', 2)
+    ]
 
-#     repository.create(Album(None, 'Mahmoud', 1987 , 2))
+"""
+When we call PostRepository#find
+We get a single Post object reflecting the seed data.
+"""
+def test_get_single_post_record(db_connection):
+    db_connection.seed("seeds/chitter_database.sql")
+    repository = PostRepository(db_connection)
 
-#     result = repository.all()
-#     assert result == [
-#         Album(1, 'Doolitle', 1989 , 1),
-#         Album(2, 'Surfer Rosa', 1988 , 2),
-#         Album(3, 'Mahmoud', 1987 , 2)
-#     ]
-    
-# """
-# When we call BookRepository#find
-# We get a single Book object reflecting the seed data.
-# """
-# def test_get_single_record(db_connection):
-#     db_connection.seed("seeds/music_web_app.sql")
-#     repository = AlbumRepository(db_connection)
-
-#     album = repository.find(1)
-#     assert album == Album(1, "Doolitle", 1989, 1)
+    post = repository.find(2)
+    assert post == Post(2, 'This is my first peep!', '2023-05-19 10:30:00', 2)
