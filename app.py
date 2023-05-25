f682bc4ef53e5de4b85a2a0498a96f1652215a0b
tiagocollot <collot.tiago1@gmail.com>
2023-05-25 17:10:32 +0100

    modifications

--- app.py
+++ app.py
@@ -1,88 +1,29 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template
+from lib.database_connection import get_flask_database_connection
+from lib.post_repository import PostRepository
+from lib.post import Post
+from lib.user_repository import UserRepository
+from lib.user import User
 
 # Create a new Flask app
 app = Flask(__name__)
 
-# == Your Routes Here ==
+@app.route('/posts')
+def get_posts():
+    connection = get_flask_database_connection(app)
+    repository = PostRepository(connection)
+    posts = repository.all()
+    return render_template("posts/index.html", posts=posts)
 
-# == Example Code Below ==
-
-# # GET /emoji
-# # Returns a emojiy face
-# # Try it:
-# #   ; curl http://localhost:5000/emoji
-# @app.route('/emoji', methods=['GET'])
-# def get_emoji():
-#     return ":)"
-
-# # Request:
-# # GET /hello?name=David
-# @app.route('/hello', methods=['GET'])
-# def hello():
-#     name = request.args['name']
-#     return f"Hello {name}!"
-# # Request:
-# # POST /goodbye
-# #   With body parameter: name=Alice
-# @app.route('/goodbye', methods=['POST'])
-# def goodbye():
-#     name = request.form['name']
-#     return f"Goodbye {name}!"
-# @app.route('/submit', methods=['POST'])
-# def submit():
-#     name = request.form['name']
-#     message = request.form['message']
-#     return f"Thanks {name}, you sent this message: {message}"
-
-# @app.route('/wave', methods=['GET'])
-# def wave():
-#     name = request.args['name']
-#     return f"I am waving at {name}"
-
-# @app.route('/count_vowels', methods=['POST'])
-# def count_vowels():
-#     text = request.form['text']
-#     vowels = ["a", "e", "i", "o", "u"]
-#     count = 0
-#     for letter in text:
-#         if letter in vowels:
-#             count += 1
-#     return f'There are {count} vowels in "{text}"'
-
-
-# @app.route('/sort_names', methods=['POST'])
-# def sort_names():
-#     try:
-#         names = request.form['names']
-#         sort_list = names.split(",")
-#         sort_list.sort()
-#         sorted_string = ",".join(sort_list)
-#         return f"These are the sorted names: {sorted_string}"
-#     except KeyError:
-#         return "Something went wrong", 400
-
-    # if 'names' in request.form:
-    #     names = request.form['names']
-    #     sort_list = names.split(",")
-    #     sort_list.sort()
-    #     sorted_string = (",").join(sort_list)
-    #     return f"These are the sorted names: {sorted_string}"
-    # else: 
-    #     return "Something went wrong", 400
-# This imports some more example routes for you to see how they work
-# You can delete these lines if you don't need them.
-
-# @app.route('/names', methods=['GET'])
-# def get_names():
-#     names = "Julia, Alice, Karim"
-#     name = request.args['add']
-#     names += f", {name}"
-#     return f"Names list: {names}"
-
-# from example_routes import apply_example_routes
-# apply_example_routes(app)
-
+@app.route('/post/<id>')
+def get_posts_with_id(id):
+    connection = get_flask_database_connection(app)
+    repository = PostRepository(connection)
+    post = repository.find(id)
+    user_repository = UserRepository(connection)
+    user = user_repository.find_with_post(post.user_id)
+    return render_template("posts/single_post.html", post=post, user=user)
 # == End Example Code ==
 
 # These lines start the server if you run this file directly
---
+++ tests/test_app.py
@@ -0,0 +1,30 @@
+from playwright.sync_api import Page, expect
+
+def test_get_posts(page, test_web_address, db_connection):
+    db_connection.seed("seeds/chitter_database.sql")
+    page.goto(f"http://{test_web_address}/posts")
+    page.screenshot(path="screenshot.png", full_page=True)
+    h2_tags = page.locator("h2")
+    paragraph_tags = page.locator("p")
+    expect(h2_tags).to_have_text([
+        'Hello, world!',
+        'This is my first peep!',
+    ])
+    expect(paragraph_tags).to_have_text([
+        'Post time: 2023-05-19 09:00:00',
+        'Post time: 2023-05-19 10:30:00'
+    ])   
+    
+def test_get_single_post(page, test_web_address, db_connection):
+    db_connection.seed("seeds/chitter_database.sql")
+    page.goto(f"http://{test_web_address}/posts/1")
+    page.screenshot(path="screenshot.png", full_page=True)
+    h2_tags = page.locator("h2")
+    paragraph_tags = page.locator("p")
+    expect(h2_tags).to_have_text([
+        'Hello, world!'
+    ])
+    expect(paragraph_tags).to_have_text([
+        'Post time: 2023-05-19 09:00:00',
+        "Artist: JohnDoe"
+    ]) 
